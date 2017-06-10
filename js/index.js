46434f34b6e76b940dd50a4fd101b820d3ebd777
Thomas Tursics <thomas@tursics.de>
2017-06-10 04:02:21 +0200

    added an optional Vollmacht

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@
 
 var gOpenIssues = [];
 var gClosedIssues = [];
+var gUser = false;
 
 //-----------------------------------------------------------------------
 
@@ -32,7 +33,11 @@ $('.navbar-nav > li').click(function (event) {
 $('#buttonGo').click(function (event) {
 	'use strict';
 
-	activateTab('#login');
+	if (gUser) {
+		activateTab('#birth');
+	} else {
+		activateTab('#login');
+	}
 });
 
 //-----------------------------------------------------------------------
@@ -42,6 +47,7 @@ $('.buttonSocialMedia').click(function (event) {
 
 	$('.navbar-right li:nth-child(1)').css('display', 'none');
 	$('.navbar-right li:nth-child(2)').css('display', 'block');
+	gUser = true;
 
 	activateTab('#city');
 });
@@ -66,6 +72,8 @@ function finishIssue(id) {
 				gOpenIssues[i].type = 'edit';
 			} else if ('paper' === gOpenIssues[i].type) {
 				gOpenIssues[i].type = 'exists';
+			} else if ('optional' === gOpenIssues[i].type) {
+				gOpenIssues[i].type = 'not';
 			}
 			gClosedIssues.push(gOpenIssues[i]);
 			gOpenIssues.splice(i, 1);
@@ -105,9 +113,14 @@ function prepareOneIssues(issue) {
 	str += '<div class="col-sm-6 col-md-4">';
 	str += '<div class="thumbnail">';
 	str += '<img alt="100%x200" data-src="holder.js/100%x200" style="height: 200px; width: 100%; display: block;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzE5IiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDMxOSAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzEwMCV4MjAwCkNyZWF0ZWQgd2l0aCBIb2xkZXIuanMgMi42LjAuCkxlYXJuIG1vcmUgYXQgaHR0cDovL2hvbGRlcmpzLmNvbQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbwotLT48ZGVmcz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPjwhW0NEQVRBWyNob2xkZXJfMTVjOGYwZDkzZmIgdGV4dCB7IGZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxNnB0IH0gXV0+PC9zdHlsZT48L2RlZnM+PGcgaWQ9ImhvbGRlcl8xNWM4ZjBkOTNmYiI+PHJlY3Qgd2lkdGg9IjMxOSIgaGVpZ2h0PSIyMDAiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMTYuOTY2NjY3MTc1MjkyOTciIHk9IjEwNy41Ij4zMTl4MjAwPC90ZXh0PjwvZz48L2c+PC9zdmc+" data-holder-rendered="true">';
-	str += '<div class="caption">';
+	str += '<div class="caption" style="position:relative;">';
 	str += '<h3>' + issue.title + '</h3>';
 	str += '<p>' + issue.comment + '</p>';
+
+	if ((issue.price > 0) && ('not' !== issue.type) && ('exists' !== issue.type)) {
+		str += '<span class="label label-danger" style="font-size:2em;transform:rotate(12deg);position:absolute;top:-.5em;right:0;">' + parseInt(issue.price, 10) + ',' + (issue.price * 100).toString().substr(-2) + ' €</span>';
+	}
+
 	str += '<p style="text-align:center;">';
 
 	if ('claim' === issue.type) {
@@ -116,10 +129,16 @@ function prepareOneIssues(issue) {
 		str += '<a href="#" class="btn btn-success" role="button" data-type="done" data-id="' + issue.id + '"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Vorhanden</a>';
 		str += '&nbsp;&nbsp;';
 		str += '<a href="#" class="btn btn-primary" role="button" data-type="edit" data-id="' + issue.id + '"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Beantragen</a>';
+	} else if ('optional' === issue.type) {
+		str += '<a href="#" class="btn btn-warning" role="button" data-type="done" data-id="' + issue.id + '"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Nein danke</a>';
+		str += '&nbsp;&nbsp;';
+		str += '<a href="#" class="btn btn-primary" role="button" data-type="edit" data-id="' + issue.id + '"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Beantragen</a>';
 	} else if ('exists' === issue.type) {
 		str += '<a href="#" class="btn btn-default disabled" role="button" data-type="done" data-id="' + issue.id + '"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Vorhanden</a>';
 	} else if ('edit' === issue.type) {
 		str += '<a href="#" class="btn btn-primary" role="button" data-type="edit" data-id="' + issue.id + '"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Bearbeiten</a>';
+	} else if ('not' === issue.type) {
+		str += '<a href="#" class="btn btn-default disabled" role="button" data-type="done" data-id="' + issue.id + '"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Nein danke</a>';
 	}
 
 	str += '</p>';
@@ -243,9 +262,10 @@ $('#buttonHospital').click(function (event) {
 	'use strict';
 
 	pushIssue('GeburtsurkundeErstbeurkundung', 'Geburtsurkunde', 'Die Geburtsurkunde für das Baby', 'claim', 10);
-	pushIssue('PersonalausweisMann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', -1);
-	pushIssue('PersonalausweisFrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', -1);
-	pushIssue('GeburtsurkundeFrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', -1);
+	pushIssue('PersonalausweisMann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('PersonalausweisFrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('GeburtsurkundeFrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', 10);
+	pushIssue('Vollmacht', 'Vollmacht', 'Eine andere Person soll die fertigen Urkunden abholen', 'optional', -1);
 
 	prepareOpenIssues();
 	activateTab('#openIssues');
@@ -258,9 +278,10 @@ $('#buttonHomeBirth').click(function (event) {
 
 	pushIssue('GeburtsurkundeErstbeurkundung', 'Geburtsurkunde', 'Die Geburtsurkunde für das Baby', 'claim', 10);
 	pushIssue('AnzeigeErklärungVorFamilienname', 'Namenserklärung', 'Welchen Vor- und welchen Nachnamen soll das Baby erhalten', 'claim', -1);
-	pushIssue('PersonalausweisMann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', -1);
-	pushIssue('PersonalausweisFrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', -1);
-	pushIssue('GeburtsurkundeFrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', -1);
+	pushIssue('PersonalausweisMann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('PersonalausweisFrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('GeburtsurkundeFrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', 10);
+	pushIssue('Vollmacht', 'Vollmacht', 'Eine andere Person soll die fertigen Urkunden abholen', 'optional', -1);
 
 	prepareOpenIssues();
 	activateTab('#openIssues');
@@ -284,6 +305,7 @@ $(document).ready(function () {
 
 	$('.navbar-right li:nth-child(1)').css('display', 'none');
 	$('.navbar-right li:nth-child(2)').css('display', 'block');
+	gUser = true;
 	activateTab('#birth');
 });
 
