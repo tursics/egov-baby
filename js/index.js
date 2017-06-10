d5a8f618ab7e37156931873c71b771d0dfc90adb
Thomas Tursics <thomas@tursics.de>
2017-06-10 04:29:45 +0200

    show document images

--- js/index.js
+++ js/index.js
@@ -112,7 +112,9 @@ function prepareOneIssues(issue) {
 
 	str += '<div class="col-sm-6 col-md-4">';
 	str += '<div class="thumbnail">';
-	str += '<img alt="100%x200" data-src="holder.js/100%x200" style="height: 200px; width: 100%; display: block;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzE5IiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDMxOSAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzEwMCV4MjAwCkNyZWF0ZWQgd2l0aCBIb2xkZXIuanMgMi42LjAuCkxlYXJuIG1vcmUgYXQgaHR0cDovL2hvbGRlcmpzLmNvbQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbwotLT48ZGVmcz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPjwhW0NEQVRBWyNob2xkZXJfMTVjOGYwZDkzZmIgdGV4dCB7IGZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxNnB0IH0gXV0+PC9zdHlsZT48L2RlZnM+PGcgaWQ9ImhvbGRlcl8xNWM4ZjBkOTNmYiI+PHJlY3Qgd2lkdGg9IjMxOSIgaGVpZ2h0PSIyMDAiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMTYuOTY2NjY3MTc1MjkyOTciIHk9IjEwNy41Ij4zMTl4MjAwPC90ZXh0PjwvZz48L2c+PC9zdmc+" data-holder-rendered="true">';
+	str += '<div class="alert alert-info" style="text-align:center;padding:.2em;margin:0;">';
+	str += '<img style="height: 200px;" src="assets/' + issue.id + '.jpg" data-holder-rendered="true">';
+	str += '</div>';
 	str += '<div class="caption" style="position:relative;">';
 	str += '<h3>' + issue.title + '</h3>';
 	str += '<p>' + issue.comment + '</p>';
@@ -261,11 +263,11 @@ $('a[href="#closedIssues"]').on('shown.bs.tab', function (e) {
 $('#buttonHospital').click(function (event) {
 	'use strict';
 
-	pushIssue('GeburtsurkundeErstbeurkundung', 'Geburtsurkunde', 'Die Geburtsurkunde für das Baby', 'claim', 10);
-	pushIssue('PersonalausweisMann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
-	pushIssue('PersonalausweisFrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
-	pushIssue('GeburtsurkundeFrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', 10);
-	pushIssue('Vollmacht', 'Vollmacht', 'Eine andere Person soll die fertigen Urkunden abholen', 'optional', -1);
+	pushIssue('geburtsurkundeerstbeurkundung', 'Geburtsurkunde beantragen', 'Die Geburtsurkunde für das Baby', 'claim', 10);
+	pushIssue('personalausweismann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('personalausweisfrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('geburtsurkundefrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', 10);
+	pushIssue('vollmacht', 'Vollmacht', 'Eine andere Person soll die fertigen Urkunden abholen', 'optional', -1);
 
 	prepareOpenIssues();
 	activateTab('#openIssues');
@@ -276,12 +278,12 @@ $('#buttonHospital').click(function (event) {
 $('#buttonHomeBirth').click(function (event) {
 	'use strict';
 
-	pushIssue('GeburtsurkundeErstbeurkundung', 'Geburtsurkunde', 'Die Geburtsurkunde für das Baby', 'claim', 10);
-	pushIssue('AnzeigeErklärungVorFamilienname', 'Namenserklärung', 'Welchen Vor- und welchen Nachnamen soll das Baby erhalten', 'claim', -1);
-	pushIssue('PersonalausweisMann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
-	pushIssue('PersonalausweisFrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
-	pushIssue('GeburtsurkundeFrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', 10);
-	pushIssue('Vollmacht', 'Vollmacht', 'Eine andere Person soll die fertigen Urkunden abholen', 'optional', -1);
+	pushIssue('geburtsurkundeerstbeurkundung', 'Geburtsurkunde beantragen', 'Die Geburtsurkunde für das Baby', 'claim', 10);
+	pushIssue('anzeigeerklaerungvorfamilienname', 'Namenserklärung', 'Welchen Vor- und welchen Nachnamen soll das Baby erhalten', 'claim', -1);
+	pushIssue('personalausweismann', 'Personalausweis des Vaters', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('personalausweisfrau', 'Personalausweis der Mutter', '... oder der Reisepass muss vorhanden sein', 'paper', 28.80);
+	pushIssue('geburtsurkundefrau', 'Geburtsurkunde der Mutter', 'Ein Nachweis, wo die Mutter geboren wurde', 'paper', 10);
+	pushIssue('vollmacht', 'Vollmacht', 'Eine andere Person soll die fertigen Urkunden abholen', 'optional', -1);
 
 	prepareOpenIssues();
 	activateTab('#openIssues');
@@ -289,10 +291,10 @@ $('#buttonHomeBirth').click(function (event) {
 
 //-----------------------------------------------------------------------
 
-$('#pageGeburtsurkundeErstbeurkundung button').click(function (event) {
+$('#pagegeburtsurkundeerstbeurkundung button').click(function (event) {
 	'use strict';
 
-	finishIssue('GeburtsurkundeErstbeurkundung');
+	finishIssue('geburtsurkundeerstbeurkundung');
 	activateTab('#openIssues');
 });
 
